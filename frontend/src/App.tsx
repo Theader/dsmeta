8e4e12daa63379ac8afacb01cd7de7cc1da86009
Theader <thiago.huebr@hotmail.com>
2022-07-17 18:08:46 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
 
   return (
-   <h1>Monkey</h1>
+    <>
+      <h1>Monkey</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
